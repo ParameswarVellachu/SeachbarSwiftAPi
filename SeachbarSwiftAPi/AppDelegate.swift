c434067e2a5596c7d47a4abb42527c6026b8bd8b
Parameswaran <appsdeveloperinfo@gmail.com>
2022-09-05 16:09:13 +0530

    Tested by dev branch

--- SeachbarSwiftAPi/AppDelegate.swift
+++ SeachbarSwiftAPi/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // params tested conflict tested by SearchBar Development branch...
         return true
     }
 
