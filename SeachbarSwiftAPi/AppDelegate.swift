9144f2450bc69bb576605f1d6947607935571e64
Parameswaran <appsdeveloperinfo@gmail.com>
2022-09-05 16:01:02 +0530

    Update AppDelegate.swift

--- SeachbarSwiftAPi/AppDelegate.swift
+++ SeachbarSwiftAPi/AppDelegate.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2018 Parameswar. All rights reserved.
 //
 
+// params tested conflict tested by SearchBar Development branch
+
 import UIKit
 
 @UIApplicationMain
@@ -16,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+
+        // params tested conflict tested by SearchBar Master branch
+
         return true
     }
 
